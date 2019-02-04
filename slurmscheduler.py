c39143278c77cefc9bc4b02c634ddb29a45fbbe4
rchensix <47170567+rchensix@users.noreply.github.com>
2019-02-04 09:22:05 -0800

    Add files via upload

--- slurmscheduler.py
+++ slurmscheduler.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 class Scheduler:
   # contains a database of submitted jobs
@@ -12,31 +13,18 @@ class Scheduler:
   # this creates a new Scheduler object with initially empty database
     self.database = dict() # this stores jobID:status pairs where jobID is a unique identifier to a submitted job, and status is 1 (completed) or 0 (else)
     
-  def sbatch(self, file, flags=""):
+  def submit(self, file, flags=""):
   # submits file using sbatch and returns (jobID, out, err); adds jobID:0 to database
   # if submission encounters an error, jobID is set to None
   # additional arguments can be specified by the flags string
   # this method will send "sbatch file flags" to the shell
   # NOTE: a space is automatically added between file and flags, but any spaces in flags must be specified in flags itself
-    popen = subprocess.Popen("sbatch " + file + flags, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = popen.communicate()
-    if len(out) != 0:
-      jobID = out.split()[-1] # NOTE: this may need to be updated in a future version to be truly unique (not sure exactly how SLURM assigns jobIDs)
-      self.database[jobID] = 0
-    else:
-      jobID = None
-    return (jobID, out, err)
-  
-  def squeue(self, flags=""):
-  # calls "squeue flags" in shell and returns output
-  # NOTE: a space is automatically added between squeue and flags, but any spaces in flags must be specified in flags itself
-    popen = subprocess.Popen("squeue " + flags, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = popen.communicate()
-    return out
-    
+    jobID, out, err = sbatch(file, flags)
+    if jobID != None: self.database[jobID] = 0
+
   def update(self):
   # updates the status for all submitted jobs by calling squeue
-    out = self.squeue()
+    out = squeue()
     enqueued = self.parseQueue(out)
     for jobID  in self.database:
       if jobID not in enqueued and self.database[jobID] == 0:
@@ -74,3 +62,86 @@ class Scheduler:
         if jobID != "JOBID":
           enqueued.add(jobID)
     return enqueued
+  
+def sbatch(file, flags=""):
+# submits file using sbatch and returns (jobID, out, err); adds jobID:0 to database
+# if submission encounters an error, jobID is set to None
+# additional arguments can be specified by the flags string
+# this method will send "sbatch file flags" to the shell
+# NOTE: a space is automatically added between file and flags, but any spaces in flags must be specified in flags itself
+  popen = subprocess.Popen("sbatch " + file + flags, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  out, err = popen.communicate()
+  if len(out) != 0:
+    jobID = out.split()[-1] # NOTE: this may need to be updated in a future version to be truly unique (not sure exactly how SLURM assigns jobIDs)
+  else:
+    jobID = None
+  return (jobID, out, err)
+  
+def squeue(flags=""):
+# calls "squeue flags" in shell and returns output
+# NOTE: a space is automatically added between squeue and flags, but any spaces in flags must be specified in flags itself
+  popen = subprocess.Popen("squeue " + flags, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  out, err = popen.communicate()
+  return out
+
+def createScript(script, outputFileName):
+  # creates a script file out of input script
+  # input can be either string or list of strings
+  # if input is string, function will take string and directly create a bash file using it (you need to include \n yourself)
+  # if input is a list of strings, function will put each element on its own line before writing to bash file (you don't need to include \n yourself)
+  # output file name is outputFileName (will automatically overwrite existing!)
+  # NOTE: no shebangs are included--put everything in script!
+  file = open(outputFileName, "w")
+  if isinstance(script, basestring):
+    file.write(script)
+  elif isinstance(script, list):
+    for line in script:
+      file.write(line + "\n")
+  else:
+    raise Exception("Unsupported input variable type used for script")
+  file.close()
+
+def createLSDynaBashScript(keyFile, directory=None, outputFile=None, outputDirectory=None, jobName=None, maxTime=None, numNode=None, numCPU=None):
+  # creates a LSDyna bash script to run on mc2 cluster
+  # if directory is not specified, current working directory is assumed
+  # if outputFile is not specified, output will be keyFile.sh
+  # if outputDirectory is not specified, directory will be used
+  # if jobName is not specified, keyFile is also used as the jobName
+  # to specify maxTime, use HH:MM:SS string format
+  # if maxTime is not specified, default maxTime will be 02:00:00 (2 hours)
+  # default numNode = 1
+  # default numCPU = 24
+  DEFAULT_MAX_TIME = "02:00:00"
+  if directory == None:
+    directory = os.getcwd()
+  if outputFile == None:
+    outputFile = keyFile + ".sh"
+  if outputDirectory == None:
+    outputDirectory = directory
+  if jobName == None:
+    jobName = keyFile
+  if maxTime == None:
+    maxTime = DEFAULT_MAX_TIME
+  if numNode == None:
+    numNode = 1
+  if numCPU == None:
+    numCPU = 24
+  script = ["#!/bin/bash",
+    "#SBATCH -J " + jobName,
+    "#SBATCH -o " + jobName + ".out",
+    "#SBATCH -t " + maxTime,
+    "#SBATCH -N " + str(numNode),
+    "#SBATCH -n " + str(numCPU),
+    "module purge",
+    "module load intel/Developer-2018.1",
+    "module load apps/ls-dyna/9.1.0",
+    "SLURM_SUBMIT_DIR = '" + outputDirectory + "'",
+    "export SLURM_SUBMIT_DIR",
+    "echo The master node of this job is `hostname`",
+    "echo The working directory is `echo $SLURM_SUBMIT_DIR `",
+    "echo This job runs on the following nodes:",
+    "echo `cat $SLURM_JOB_NODELIST `",
+    "cd $SLURM_SUBMIT_DIR",
+    "ls-dyna_smp_s_r910_x64_redhat56_ifort131 I= " + keyFile + " NCPU= " + str(numCPU)]
+  fullPath = os.path.join(outputDirectory, outputFile)
+  createScript(script, fullPath)
